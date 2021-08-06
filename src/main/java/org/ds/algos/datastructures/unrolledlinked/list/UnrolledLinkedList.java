3faaf2b40372bd51410d7c7b6857d47446c4abf1
ds algos <ds.algos143@gmail.com>
2021-08-06 23:14:00 +0530

    Commit code
    Add implementation of remove method
    Adding some generic code using Pair Java class.

--- src/main/java/org/ds/algos/datastructures/unrolledlinked/list/UnrolledLinkedList.java
+++ src/main/java/org/ds/algos/datastructures/unrolledlinked/list/UnrolledLinkedList.java
@@ -5,6 +5,8 @@ import java.util.AbstractList;
 import java.util.Iterator;
 import java.util.List;
 
+import javafx.util.Pair;
+
 public class UnrolledLinkedList<E> extends AbstractList<E> implements List<E>, Serializable {
 
 	/**
@@ -36,91 +38,138 @@ public class UnrolledLinkedList<E> extends AbstractList<E> implements List<E>, S
 		insertIntoNode(this.tail, this.tail.numElements, element);
 		return true;
 	}
-	
+
 	@Override
 	public void add(int index, E element) {
-		
+
 		if(index < 0 || index > this.size) {
 			throw new ArrayIndexOutOfBoundsException(index);
 		}
-		
-		BlockNode<E> node;
-		int ptr;
-		//search from left to right 		
-		if((this.size - index) >= index) {
-			node = this.head;
-			for(ptr = 0; (ptr < (index - node.numElements)); ptr+= node.numElements) {
-				node = node.next;				
-			}						
-		} else {
-			node = this.tail;
-			for(ptr = this.size; (ptr -= node.numElements) > index; ) {
-				node = node.prev;
-			}
-		}
-		
-		System.out.println(" index : " + index + " ptr : " + ptr);
-		insertIntoNode(node, index - ptr, element);
-		
+
+		/*
+		 * BlockNode<E> node; int ptr; //search from left to right if((this.size -
+		 * index) >= index) { node = this.head; for(ptr = 0; (ptr < (index -
+		 * node.numElements)); ptr+= node.numElements) { node = node.next; } } else {
+		 * node = this.tail; for(ptr = this.size; (ptr -= node.numElements) > index; ) {
+		 * node = node.prev; } }
+		 * 
+		 * System.out.println(" index : " + index + " ptr : " + ptr);
+		 */		
+		Pair<BlockNode<E>, Integer> nodePtrPair = getNodeAndPointerToIndex(index);		
+		insertIntoNode(nodePtrPair.getKey(), index - nodePtrPair.getValue(), element);
+
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	@Override
 	public E set(int index, E element) {
 		if(index < 0 || index > size) {
 			throw new ArrayIndexOutOfBoundsException(index);
 		}
+
+		Pair<BlockNode<E>, Integer> nodePtrPair = getNodeAndPointerToIndex(index);		
+
+		int position = index - nodePtrPair.getValue();
+
+		BlockNode<E> node = nodePtrPair.getKey();
+		E oldElement = (E) node.elements[position];		
+		node.elements[position] = element;
+		modCount++;
+
+		return oldElement;
+	}
+
+	@Override
+	public boolean remove(Object o) {
+
+		if(isEmpty()) { return false; }
+
+		BlockNode<E> node = this.head;
+		int index = 0;
+		if(o == null) {
+			while(node != null) {
+
+				for(int ptr = 0; ptr < node.numElements; ptr++) {
+					if(node.elements[ptr] == null) {
+						removeNode(node, index + ptr);
+						return true;
+					}
+				}
+
+				index += node.numElements;
+				node = node.next;
+			}
+		} else {
+
+			while(node != null) {
+
+				for(int ptr = 0; ptr < node.numElements; ptr++) {
+					if(node.elements[ptr] == null) {
+						removeNode(node, index + ptr);
+						return true;
+					}
+				}
+
+				index += node.numElements;
+				node = node.next;
+			}
+		}
+
+		return false;
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public E remove(int index) {
+
+		if(index < 0 || index >  this.size) {
+			throw new ArrayIndexOutOfBoundsException(index);
+		}
 		
 		int ptr = 0;
 		BlockNode<E> node = this.head;
-		if((this.size - index >= index)) {
-			//add to ptr the number of elements current node has
-			//such that once the node is identified, correct position can 
-			//be calculated using (index - ptr) equation
-			for( ;(ptr+node.numElements) < index; ptr+=node.numElements) {
+		
+		if((this.size - index) > index) {
+			while(ptr + node.numElements <= index) {
+				ptr += node.numElements;
 				node = node.next;
 			}
 			
 		} else {
 			node = this.tail;
-			//subtract from ptr the number of elements current node has
-			//such that once the node is identified, correct position can 
-			//be calculated using (index - ptr) equation
-			for(ptr = this.size; (ptr -= node.numElements) > index; ) { 
+			ptr = this.size;
+			while((ptr -= node.numElements) > index) {
 				node = node.prev;
 			}
 		}
 		
-		int position = index - ptr;
+		E element = (E) node.elements[index-ptr];
+		removeNode(node, index - ptr);
+		return element;
 		
-		E oldElement = (E) node.elements[position];		
-		node.elements[position] = element;
-		modCount++;
-		
-		return oldElement;
 	}
-	
+
 	@Override
 	public int indexOf(Object o) {
-		
+
 		if(isEmpty()) { return -1; }
-		
+
 		int index = 0;
 		BlockNode<E> node = this.head;
-		
+
 		if(o == null) {
-			
+
 			while(node != null) {
 				for(int ptr = 0; ptr < node.numElements; ptr++) {
 					if(node.elements[ptr] == null) {
 						return (index + ptr);
 					}					
 				}
-				
+
 				index += node.numElements;
 				node = node.next;
 			}
-			
+
 		} else {
 			while(node != null) {
 				for(int ptr = 0; ptr < node.numElements; ptr++) {
@@ -128,32 +177,30 @@ public class UnrolledLinkedList<E> extends AbstractList<E> implements List<E>, S
 						return (index + ptr);
 					}
 				}
-				
+
 				index += node.numElements;
 				node = node.next;
 			}
 		}
-		
-		
-		
+
 		return -1;
 	}
-	
+
 	@Override
 	public boolean contains(Object o) {
 		return (indexOf(o) != -1);
 	}
-	
+
 	@Override
 	public String toString() {
 		if(isEmpty()) {
 			System.out.println("List is Empty");
 			return "{ [ ] }";
 		}
-		
+
 		StringBuilder str = new StringBuilder("{ ") ;
 		BlockNode<E> fwd = this.head;
-		
+
 		while(fwd != null) {
 			str.append("[ ");
 			for(int index = 0; index < fwd.numElements; index++) {
@@ -165,7 +212,7 @@ public class UnrolledLinkedList<E> extends AbstractList<E> implements List<E>, S
 			str.append("]");
 			fwd = fwd.next;
 		}
-		
+
 		str.append(" }");
 		return str.toString();
 	}
@@ -193,28 +240,72 @@ public class UnrolledLinkedList<E> extends AbstractList<E> implements List<E>, S
 		return (this.size == 0 || this.head == null);
 	}
 
+
+	private void removeNode(BlockNode<E> node, int index) {
+		
+		int ptr = index;
+		for(; ptr < node.numElements-1; ptr++) {
+			node.elements[ptr] = node.elements[ptr+1];			
+		}
+		
+		node.elements[ptr+1] = null;
+		node.numElements--;
+		
+		if (node.next != null && node.next.numElements + node.numElements <= nodeCapacity) {
+            mergeWithNextNode(node);
+        } else if (node.prev != null && node.prev.numElements + node.numElements <= nodeCapacity) {
+            mergeWithNextNode(node.prev);
+        }
+		
+		size--;
+		modCount++;
+	}
+	
+	 /**
+     * This method does merge the specified node with the next node.
+     *
+     * @param node the node which should be merged with the next node
+     */
+    private void mergeWithNextNode(BlockNode<E> node) {
+
+        BlockNode<E> next = node.next;
+        for (int i = 0; i < next.numElements; i++) {
+            node.elements[node.numElements + i] = next.elements[i];
+            next.elements[i] = null;
+        }
+        node.numElements += next.numElements;
+        if (next.next != null) {
+            next.next.prev = node;
+        }
+        node.next = next.next;
+        if (next == this.tail) {
+            this.tail = node;
+        }
+
+    }
+
 	private void insertIntoNode(BlockNode<E> node, int ptr, E element) {
 
 		//if capacity is full for the current tail node
 		//half of the elements have to be shifted to the new node
 		if(node.numElements == this.nodeCapacity) {
 			BlockNode<E> newNode = new BlockNode<E>(this.nodeCapacity);
-			
+
 			int elementsToBeMoved = this.nodeCapacity / 2;
 			int startIndex = this.nodeCapacity - elementsToBeMoved;
-			
+
 			for(int i = 0; i < elementsToBeMoved; i++) {
 				newNode.elements[i] = node.elements[startIndex + i];
 				node.elements[startIndex + i] = null;
 			}
-			
+
 			//update size of the node
 			node.numElements -= elementsToBeMoved;
 			newNode.numElements = elementsToBeMoved;
-			
+
 			newNode.next = node.next;			
 			newNode.prev = node;
-			
+
 			if(node.next != null) {
 				node.next.prev = newNode;				
 			} 
@@ -222,7 +313,7 @@ public class UnrolledLinkedList<E> extends AbstractList<E> implements List<E>, S
 			if(node == this.tail) {
 				this.tail = newNode;
 			}
-			
+
 			//check whether the element should be inserted into
 			//the original node or into the new node
 			if(ptr > node.numElements) {
@@ -230,7 +321,7 @@ public class UnrolledLinkedList<E> extends AbstractList<E> implements List<E>, S
 				ptr -= node.numElements; //to calculate the index where the element has to be inserted.
 			}
 		}
-		
+
 		for(int i = node.numElements; i > ptr ;i--) {
 			node.elements[i] = node.elements[i-1];			
 		}
@@ -241,14 +332,36 @@ public class UnrolledLinkedList<E> extends AbstractList<E> implements List<E>, S
 		modCount++;				
 	}
 
+	private Pair<BlockNode<E>, Integer> getNodeAndPointerToIndex(int index) {
+		if(index < 0 || index > this.size) {
+			throw new ArrayIndexOutOfBoundsException(index);
+		}
+
+		BlockNode<E> node;
+		int ptr;
+		//search from left to right 		
+		if((this.size - index) >= index) {
+			node = this.head;
+			for(ptr = 0; (ptr < (index - node.numElements)); ptr+= node.numElements) {
+				node = node.next;				
+			}						
+		} else {
+			node = this.tail;
+			for(ptr = this.size; (ptr -= node.numElements) > index; ) {
+				node = node.prev;
+			}
+		}
+
+		return new Pair<BlockNode<E>, Integer>(node, ptr);
+	}	
 
 
-	static class BlockNode<T> {
+	static class BlockNode<E> {
 
 		int numElements;
 		Object elements[];
-		BlockNode<T> next;
-		BlockNode<T> prev;	
+		BlockNode<E> next;
+		BlockNode<E> prev;	
 
 
 		public BlockNode(int capacity) {
@@ -272,9 +385,9 @@ public class UnrolledLinkedList<E> extends AbstractList<E> implements List<E>, S
 
 			return null;
 		}
-		
+
 		public void remove() {
-			
+
 		}
 	}
 }
